6c901af04573fd60e377adf95ce9516787cdd443
jsmo93 <jsmo93@gmail.com>
2015-04-16 14:07:05 -0400

    Added object-oriented handler

--- Data-Structure-GUI-Core.rkt
+++ Data-Structure-GUI-Core.rkt
@@ -7,40 +7,79 @@
 (require "Table-Optimization.rkt")
 (require "Table-Sorting.rkt")
 
-(define master-ds null)
-(define master-table null)
-(define master-target null)
-(define master-dc null)
-
-(provide load)
-(define (load data-structure)
-  (define initial-table (load-list data-structure))
-  (define table-compress-rows (compress-rows initial-table))
-  (define table-col-sort (sort-table-cols
-                          table-compress-rows
-                          null
-                          (table-row-count table-compress-rows)
-                          (table-col-count table-compress-rows)))
-  (define tabfinal (sort-table-rows
-                    (compress-cols table-col-sort)
-                    null
-                    (table-row-count table-col-sort)
-                    (table-col-count table-col-sort)))
-  (set! master-table tabfinal)
-  master-target)
-
-(provide draw)
-(define (draw)
-  (set! master-target (make-target (table-row-count master-table) (table-col-count master-table)))
-  (set! master-dc (make-dc master-target))
-  (draw-table master-table master-dc))
-
-(provide draw-enclosed)
-(define (draw-enclosed)
-  (set! master-target (make-target (table-row-count master-table) (table-col-count master-table)))
-  (set! master-dc (make-dc master-target))
-  (draw-enclosed-table master-table master-dc))
-
-(provide save)
-(define (save)
-  (save-helper master-table (table-entry-rc 0 0 master-table)))
+(provide data-structure-core)
+(define (data-structure-core data-structure)
+  (define master-ds null)
+  (define master-table null)
+  (define master-target null)
+  (define master-dc null)
+  
+  (define (load-data-structure)
+    (define table-uc (load-list master-ds))
+    (define table-c
+      (sort-table-rows
+       (compress-cols
+        (sort-table-cols
+         (compress-rows
+          table-uc)))))
+    (set! uncompressed-table table-uc)
+    (set! master-table table-c))
+  
+  (define (draw-data-structure)
+    (set! master-target (make-target master-table))
+    (set! master-dc (make-dc master-target))
+    (draw-table master-table master-dc))
+  
+  (define (draw-enclosed-data-structure)
+    (set! master-target (make-target master-table))
+    (set! master-dc (make-dc master-target))
+    (draw-enclosed-table master-table master-dc))
+  
+  (define (save-data-structure)
+   (define new-ds (save-helper master-table (table-entry-rc 0 0 master-table)))
+    (set! master-ds new-ds)
+    new-ds)
+  
+  (define (display-help)
+    (begin
+      (write "Supported Commands:")
+      (newline)
+      (write "draw - Draws the data structure")
+      (newline)
+      (write "draw-enclosed - Draws the data structure with gridlines")
+      (newline)
+      (write "save - Saves the data structure and returns it")
+      (newline)))
+  
+  (define (dispatch m)
+    (cond ((eq? m 'draw) (begin (draw-data-structure) master-target))
+          ((eq? m 'draw-enclosed) (begin (draw-enclosed-data-structure) master-target))
+          ((eq? m 'draw-uncompressed) (begin (draw-uncompressed-data-structure) uncompressed-target))
+          ((eq? m 'draw-enclosed-uncompressed) (begin (draw-enclosed-uncompressed-data-structure) uncompressed-target))
+          ((eq? m 'help) (display-help))
+          ((eq? m 'load) (load-data-structure))
+          ((eq? m 'peek-table) master-table)
+          ((eq? m 'peek-ds) master-ds)
+          ((eq? m 'peek-uncompressed) uncompressed-table)
+          ((eq? m 'save) (save-data-structure))
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                       m))))
+    
+  ;Debugging structures
+  (define uncompressed-table null)
+  (define uncompressed-target null)
+  (define uncompressed-dc null)
+  
+  (define (draw-uncompressed-data-structure)
+    (set! uncompressed-target (make-target uncompressed-table))
+    (set! uncompressed-dc (make-dc uncompressed-target))
+    (draw-table uncompressed-table uncompressed-dc))
+  
+  (define (draw-enclosed-uncompressed-data-structure)
+    (set! uncompressed-target (make-target uncompressed-table))
+    (set! uncompressed-dc (make-dc uncompressed-target))
+    (draw-enclosed-table uncompressed-table uncompressed-dc))
+  
+  (set! master-ds data-structure)
+  (load-data-structure)
+  dispatch)
--- OPL-Project.rkt
+++ OPL-Project.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require racket/gui)
+(require "Data-Structure-GUI-Core.rkt")
 (require "Diagram-Components.rkt")
 (require "Diagram-Drawing.rkt")
 (require "Load-Save-Utils.rkt")
@@ -9,18 +10,4 @@
 (require "Table-Sorting.rkt")
 
 (define ds (list (list 1 2) (list 3 4 5) 6))
-(define uncompressed-table (load-list ds))
-(define compressed-table
-  (sort-table-rows
-   (compress-cols
-    (sort-table-cols
-     (compress-rows
-      (load-list ds))))))
-
-(define uc-target (make-target uncompressed-table))
-(define c-target (make-target compressed-table))
-(define uc-dc (make-dc uc-target))
-(define c-dc (make-dc c-target))
-
-(draw-enclosed-table uncompressed-table uc-dc)
-(draw-enclosed-table compressed-table c-dc)
+(define gui-core (data-structure-core ds))
